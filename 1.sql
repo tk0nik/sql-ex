8cffcb58a722d4720dd5afc57e7b3fa945cea6a4
tk0nik <72498532+tk0nik@users.noreply.github.com>
2021-11-16 15:56:37 +0300

    Create 1.sql
    task number 1

---
+++ 1.sql
@@ -0,0 +1,13 @@
+/*
+------------
+The database scheme consists of four tables:
+Product(maker, model, type)
+PC(code, model, speed, ram, hd, cd, price)
+Laptop(code, model, speed, ram, hd, screen, price)
+Printer(code, model, color, type, price)
+------------
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+Result set: model, speed, hd.*/
+
+select model, speed, hd
+from PC where price <500
