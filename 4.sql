4306bdec5bf19bde2953aced0b5489ade3bf2dda
tk0nik <72498532+tk0nik@users.noreply.github.com>
2021-11-24 00:19:39 +0300

    Exercise: 4
    select, where

---
+++ 4.sql
@@ -0,0 +1,10 @@
+/*
+The database scheme consists of four tables:
+Product(maker, model, type)
+PC(code, model, speed, ram, hd, cd, price)
+Laptop(code, model, speed, ram, hd, screen, price)
+Printer(code, model, color, type, price)
+
+Exercise: 4 
+Find all records from the Printer table containing data about color printers.
+*/
